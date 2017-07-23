6d125755821718c5437da7445de14c47973e57a9
Seenivasan Sankaran <seenivasan.s@heymath.com>
2017-07-23 12:15:27 +0530

    updated

--- public/assets/js/invController.js
+++ public/assets/js/invController.js
@@ -16,6 +16,8 @@ z.controller('contentControl',function($scope,$translate,$rootScope){
 	$scope.H7 =$translate.instant("H7");
 
 	$scope.first_meet =$translate.instant("first_meet");
+	$scope.h3_head =$translate.instant("h3_head");
+
 
 	$scope.Hero_Name =$translate.instant("Hero_Name");
       $scope.Heroine_Name=$translate.instant("Heroine_Name");
@@ -26,6 +28,41 @@ z.controller('contentControl',function($scope,$translate,$rootScope){
 			$scope.bride_intro=$translate.instant("bride_intro");
 
      $scope.Second_Heading=$translate.instant("Second_Heading");
+		 $scope.phone_conv=$translate.instant("phone_conv");
+		 $scope.gift=$translate.instant("gift");
+		 $scope.engagement=$translate.instant("engagement");
+		 $scope.HH1=$translate.instant("HH1");
+		 $scope.HH2=$translate.instant("HH2");
+		 $scope.HH3=$translate.instant("HH3");
+		 $scope.HH4=$translate.instant("HH4");
+
+		 $scope.HH1=$translate.instant("HH1");
+ 		 $scope.HH2=$translate.instant("HH2");
+ 		 $scope.HH3=$translate.instant("HH3");
+ 		 $scope.HH4=$translate.instant("HH4");
+     $scope.HH5=$translate.instant("HH5");
+     $scope.HH6=$translate.instant("HH6");
+
+		 $scope.r11=$translate.instant("r11");
+		 $scope.r12=$translate.instant("r12");
+		 $scope.r13=$translate.instant("r13");
+
+		 $scope.r21=$translate.instant("r21");
+		 $scope.r22=$translate.instant("r22");
+		 $scope.r23=$translate.instant("r23");
+
+		 $scope.r31=$translate.instant("r31");
+		 $scope.r32=$translate.instant("r32");
+		 $scope.r33=$translate.instant("r33");
+
+		 $scope.r41=$translate.instant("r41");
+		 $scope.r42=$translate.instant("r42");
+		 $scope.r43=$translate.instant("r43");
+
+		 $scope.r51=$translate.instant("r51");
+		 $scope.r52=$translate.instant("r52");
+		 $scope.r53=$translate.instant("r53");
+
      $scope.Line_One=$translate.instant("Line_One");
       $scope.Sub_Quote=$translate.instant("Sub_Quote");
       $scope.Label_Hero=$translate.instant("Label_Hero");
@@ -72,6 +109,38 @@ z.controller('contentControl',function($scope,$translate,$rootScope){
 			$scope.groom_intro=$translate.instant("groom_intro");
 			$scope.bride_intro=$translate.instant("bride_intro");
 			$scope.first_meet =$translate.instant("first_meet");
+			$scope.h3_head =$translate.instant("h3_head");
+			$scope.phone_conv=$translate.instant("phone_conv");
+			$scope.gift=$translate.instant("gift");
+
+			$scope.engagement=$translate.instant("engagement");
+ 		 $scope.HH1=$translate.instant("HH1");
+ 		 $scope.HH2=$translate.instant("HH2");
+ 		 $scope.HH3=$translate.instant("HH3");
+ 		 $scope.HH4=$translate.instant("HH4");
+     $scope.HH5=$translate.instant("HH5");
+     $scope.HH6=$translate.instant("HH6");
+
+		 $scope.r11=$translate.instant("r11");
+		 $scope.r12=$translate.instant("r12");
+		 $scope.r13=$translate.instant("r13");
+
+		 $scope.r21=$translate.instant("r21");
+		 $scope.r22=$translate.instant("r22");
+		 $scope.r23=$translate.instant("r23");
+
+		 $scope.r31=$translate.instant("r31");
+		 $scope.r32=$translate.instant("r32");
+		 $scope.r33=$translate.instant("r33");
+
+		 $scope.r41=$translate.instant("r41");
+		 $scope.r42=$translate.instant("r42");
+		 $scope.r43=$translate.instant("r43");
+
+		 $scope.r51=$translate.instant("r51");
+		 $scope.r52=$translate.instant("r52");
+		 $scope.r53=$translate.instant("r53");
+
 
      $scope.Second_Heading=$translate.instant("Second_Heading");
      $scope.Line_One=$translate.instant("Line_One");
