a222d99424ff56867ed02578c3e76dc19a241b45
Seenivasan <mailtonivass@gmail.com>
2017-07-27 22:54:46 +0530

    updated

--- public/assets/js/language.js
+++ public/assets/js/language.js
@@ -4,12 +4,12 @@ z.constant('languages',{
   en: function() {
     var english = {
 
-	  'Title': 'Seenu - Raji Wedding',
+	'Title': 'Seenu - Raji Wedding',
 
     'H1': 'Home',
     'H2':'Couple',
     'H3': 'Love Story',
-    'H4': 'People',
+    'H4': 'Important People',
     'H5': 'Events',
     'H6': 'Gallery',
     'H7': 'RSVP',
@@ -20,8 +20,8 @@ z.constant('languages',{
       'Label_Fun': 'A Wedding Shower',
       'Top_Quote': 'Marriage is not only the beginning of a relationship, but also the end of life long search for the other half of your soul! \n We found each others...',
 
-      'groom_intro':'From Sept 4 forward, you shall not walk alone my heart will be your shelter and my arms will be your home darling... \n Myself, S.Seenivasan, son of M.Sankaran and Selvarani. Getting married with my Sivagiri girl...',
-      'bride_intro':'I have found the one whom my soul loves, Hold my hand forever and let\'s start our life journey together.. \n Myself, M.Rajalakshmi daughter of N. Mariappan and A.Usha.Loved to be Mrs.Seenu',
+      'groom_intro':'From Sept 4 forward, you shall not walk alone my heart will be your shelter and my arms will be your home darling... \n Myself, S.Seenivasan, son of M.Sankaran and S.Selvarani. Getting married with my Sivagiri girl...',
+      'bride_intro':'I have found the one whom my soul loves, Hold my hand forever and let\'s start our life journey together.. \n Myself, M.Rajalakshmi daughter of N. Mariappan and A.Usha. Loved to be Mrs.Seenu',
       'h3_head':'A great marriage is not when the “perfect couple” comes together. It is when an imperfect couple learns to enjoy their differences...\n It is our parents who have fixed and made a bond between us.... Now it is our duty to begin a new love story.. ',
 
       'HH1':'First Meet',
@@ -31,10 +31,10 @@ z.constant('languages',{
       'HH5':'Engagement',
       'HH6':'Happy Moments',
 
-      'first_meet':'In our first meet, we were excited to knew about each other. We got some time to talk. \n The conversation started with just hi ,hello like that and after sometime We both impressed by each other.',
+      'first_meet':'In our first meet, we were excited to know about each other. We got some time to talk. \n The conversation started with just hi ,hello like that and after sometime We both impressed by each other.',
       'phone_conv':'After our first meet we both eager to start chatting. By all means I got her number finally. When I started to talk with her,she said that she had my number already and she waited for my first message.',
       'gift':'Eventhough we had phone conversation but it was not clear due to bad Vodafone signal in sivagiri. So he gifted me a new mobile phone with sim card and I presented him a watch.',
-      'engagement':'3rd July 2017 the day of betrothal ceremony, Our marriage date has been fixed with the blessings of all on 4 th September 2017.. \n And we were happily engaged. Relatives and friends from both side were also happy on this occasion',
+      'engagement':'3rd July 2017 the day of betrothal ceremony, Our marriage date has been fixed with the blessings of all on 4 th September 2017 and we were happily engaged. Relatives and friends from both side were also happy on this occasion.',
 
       "rs2":"We are missing your present. Don’t be late no more.",
 
@@ -54,18 +54,18 @@ z.constant('languages',{
       'r41': 'Manthiram - Gomathi',
       'r42': 'Groom brother - Sister in law',
 
-      'r33': '(Brother in Law) I am happy that my brother in law seenu is getting married with raji.. By this glorious moment I get on more sister along with Gomathi. \n(Sister) I am in excite mood that my beloved brother gonna be come a family man. Hereafter I will be showered with love and affection from both my brother and sister in law',
+      'r33': '(Brother in Law) I am happy that my brother in law seenu is getting married with raji.By this glorious moment I get one more sister along with Gomathi. \n(Sister) I am in excite mood that my beloved brother gonna become a family man. Hereafter I will be showered with love and affection from both my brother and sister in law',
       'r43': '(Brother) Dear seenu, I am feeling blessed to have you as my Brother. For sis - in - law, Thanks for coming our way. My heart wishes to both of you.. After the arrival of gomathi,this is the first cheerful function in our family... So we are enjoying..... \n(Sister in Law) Very happy for both my brother in law and new sister... Stay blessed with love.....',
 
       'r51': 'Arun Kalyan',
       'r52': 'Bride Brother',
-      'r53': 'Welcome you brother in law to our happy family. I am so excited... Nd strongly believe u both are perfect man and women. \n Wishing you both long life and unconditional happiness forever.. Love you both and Gonna miss you sis badly.',
+      'r53': 'Welcome you brother in law to our happy family. I am so excited and strongly believe u both are perfect man and women. \n Wishing you both long life and unconditional happiness forever.. Love you both and Gonna miss you sis badly.',
 
       'e11':'Love is there to bind us together and, we wish the time stays like that forever!.\n We are getting married. \n Do us the honour by coming to the function On \n Monday, September 4 at 6.00 - 7.30 \n in the morning. \n\n Place: Siva Kalyana Mandapam, Tenkasi',
-      'e12':'We want to share our happiness with the inmates of pranav aashrama\'s children and old aged people on our wedding day. \n\n Date: Monday, September 4, 2017. \n Time: 1 - 2 Pm \n\n\n Place: Pranav Asiramam, Courtralam.',
-      'e13':'Let\'s wrap our best wishes and shower the bride and groom with love... Please join us for the reception.. \n On Monday, \n September 4 at 6.00 - 7.30 \n in the evening. \n\n\n Place: Siva Kalyana Mandapam, Tenkasi',
+      'e12':'We want to share our happiness with the inmates of pranav aashrama\'s children and old aged people on our wedding day. \n\n Date: Monday, September 4, 2017. \n Time: 1 - 2 Pm \n\n\n Place - Pranav Ashiramam, Courtralam.',
+      'e13':'Let\'s wrap our best wishes and shower the bride and groom with love... Please join us for the reception.. \n On Monday, \n September 4 at 6.00 - 7.25 \n in the evening. \n\n\n Place - Siva Kalyana Mandapam, Tenkasi',
       'rs5':'Be a part of the festivities and brace yourselves for a whole lot of fun.',
-      'rs3':'Those special memories of us will bring a smile on our Face... Forever...And remains love with us...',
+      'rs3':'Those special memories of us will bring a smile on our face forever and remains love with us...',
       'e0':'"Marriage is caring and sharing, giving and forgiving, loving and being loved, walking hand in hand, \n taking heart to heart, to give and to take, and to see through each other eyes"',
       'e1':'Wedding Ceremony',
       'e2':'Wedding Celebration',
@@ -171,13 +171,13 @@ z.constant('languages',{
       '\n இந்நாளும் எந்நாளும், கை கோர்த்துப் போகும் பாதை, கண்ணில் தோன்றுதே! சொல்லாமல் கொள்ளாமல், நெஞ்சோடு காதல் சேர, மூச்சு முட்டுதே!', //'வாழ்த்துவோம் வாங்க',
 
       'Second_Heading': 'மணமக்கள்',
-      'groom_intro':'இவர்தாங்க நம்ம மாப்பிள்ளை பேரு சீனு. இவருக்குத்தான் டும் டும் டும் பீ பீ பீ. திருநெல்வேலி பையன்தான் வீட்டுக்கு செல்ல பிள்ளை, சில சமயம் வீட்டு பேச்சை கேக்குற நல்ல பிள்ளை. அப்பிடி கேட்டுத்தான் சிவகிரிக்கு பொண்ணு பாக்க போயி அங்க அவன் மனசு பண்ணின சேட்டைனால இப்போ சிவகிரிக்கு மாப்பிளையாக போறான்',
+      'groom_intro':'இவர்தாங்க நம்ம மாப்பிள்ளை பேரு சீனு. இவருக்குத்தான் டும் டும் டும் பீ பீ பீ. திருநெல்வேலி பையன்தான் வீட்டுக்கு செல்ல பிள்ளை, சில சமயம் வீட்டு பேச்சை கேக்குற நல்ல பிள்ளை. அப்படி கேட்டுத்தான் சிவகிரிக்கு பொண்ணு பாக்க போயி அங்க அவன் மனசு பண்ணின சேட்டைனால இப்போ சிவகிரிக்கு மாப்பிளையாக போறான்',
       'bride_intro':'இவங்கதான் நம்ம மாப்பிளைய சிரிச்சு கவுத்த சிவகிரி பொண்ணு பேரு ராஜீ. இவர்களுக்கும் சீனுக்கும்தான் டும் டும் டும் பீ பீ பீ. வீட்டுக்கு முதல் பொண்ணு அதுனால கேக்கவா செய்யணும் செல்லம் ரொம்ப ஜாஸ்திதான். இவங்களுக்கு நம்ம மாப்பிளையை பிடிச்சனால திருநெல்வேலிக்கு மருமகளாக வர போறா',
       'h3_head':'ஒவ்வொரு காதல் கதையும் ரொம்ப அழகானது ஆனா எங்க கதை எங்களுக்கு ரொம்ப ரொம்ப பிடிச்சது. \n எங்க கல்யாணம் வீட்ல எல்லாரும் பேசி, எங்க ரெண்டு பேரோட வாழ்க்கைத் துணைங்கிற கேள்விக்கு இதுதான் பொண்ணு இதுதான் மாப்பிள்ளைன்னு அவங்களே பதில் எழுதிட்டாங்க, \n அதுக்கு அப்புறமாதாங்க நாங்க ஆரம்பிச்சோம் எங்க காதல் கதையை !!!.',
       'first_meet':'ரெண்டு குடும்பத்துக்கும் எங்கள பிடிச்சதால, அடுத்து  பொண்ணு பாக்குற நாளா இல்லாம அது எங்க முதல் சந்திப்பாயிடுச்சு. எங்க ரெண்டுபேருக்கும் ஒரே குழப்பம் , எனக்கு அவள் எப்பிடி இருக்க போறான்னும்? அவளுக்கு என்னை பிடிக்குமானு கேள்விலதான் நாங்க  ஹலோ ஹாய்னு ஆரம்பிச்சோம். எங்க பேச்சு முடிவுல நான் ராஜீயோட சிரிப்புல விழுந்தேன், அவ என்னோட பேச்சுல விழுந்துட்டா',
-      'phone_conv':'நேர்ல பேசுனா அப்புறம் ரெண்டு பேருக்கும் பிடிச்சு போச்சு, அதுக்கப்புறமா நான் அடுத்த வேலைய ஆரம்பிச்ச, அது என்னன்னா கஷ்டப்பட்டு உன்  மொபைல் நம்பர வாங்கிருக்கேன்னு சொன்னா, "அப்பம்  சொல்ரா உங்க நம்பர் ஏற்கனவே எண்ட இருக்குனு, அப்பம ஏன் பேசலனு கேட்டா, உங்க மெசேஜ்க்குதான் காத்துட்டு இருக்கன்னு சொல்ற". அப்பதான் புரிஞ்சுது  இப்டி சுத்த விடறாளேனு, அதுக்கு அப்புறம் யோசிச்சன்  "ஐய்யோ கடவுளே நாம இவளை எப்படி சமாளிக்க போறன்னு"',
+      'phone_conv':'நேர்ல பேசுனா அப்புறம் ரெண்டு பேருக்கும் பிடிச்சு போச்சு, அதுக்கப்புறமா நான் அடுத்த வேலைய ஆரம்பிச்ச, என்னன்னா கஷ்டப்பட்டு உன்  மொபைல் நம்பர வாங்கிருக்கேன்னு சொன்னா, "அப்போ சொல்றா உங்க நம்பர் ஏற்கனவே எங்கிட்ட இருக்குனு, அப்போ ஏன் பேசலனு கேட்டா, உங்க மெசேஜ்க்குதான் காத்துட்டு இருக்கேன்னு சொல்றா". அப்பதான் புரிஞ்சுது  இப்டி சுத்த விடறாளேனு, அதுக்கு அப்புறம் யோசிச்சேன் "ஐய்யோ கடவுளே நாம இவளை எப்படி சமாளிக்க போறன்னு"',
       'gift':'என்னதான் நம்பர் வாங்குனாலும், எங்களுக்கு சிவகிரில வோடோபோன் சிக்னல் எல்லாம் விரோதியா இருந்துச்சு, பேசுறப்பம்ல பாதி நேரம் கேக்குதா கேக்குதானே கேட்டு நேரம் போச்சு. அதுனால நான் ஒரு மொபைல் வாங்கி அதுல ஒரு புது சிம் சேர்த்து கொடுத்தேன் , அவ எனக்கும் மேல யோசிச்சு நான் நேரத்துக்கு பேசணுமேனு  ஒரு கை கடிகாரம் கொடுத்தா...',
-      'engagement':'கடைசியா நாங்க எதிர் பார்த்த நாளும் வந்துச்சு, அதான் எங்க நிச்சயதார்த்தம்தான். செப்டம்பர் 4 கல்யாணம்னு முடிவு பண்ணி அறிவிச்சாங்க, எல்லாரும் செம சந்தோசமா இருந்தாங்க நாங்களும்தான் , மாத்தி மாத்தி பரிசு குடுத்துகுட்டோம், இந்த மகிழ்ச்சியான தருணத்தை அழகான நினைவுகளை மாற்ற, புகைப்படம் எல்லாம் எடுத்தோம்.',
+      'engagement':'கடைசியா நாங்க எதிர் பார்த்த நாளும் வந்துச்சு, அதான் எங்க நிச்சயதார்த்தம்தான். செப்டம்பர் 4 கல்யாணம்னு முடிவு பண்ணி அறிவிச்சாங்க, எல்லாரும் செம சந்தோசமா இருந்தாங்க நாங்களும்தான் , மாத்தி மாத்தி பரிசு குடுத்துகுட்டோம், இந்த மகிழ்ச்சியான தருணத்தை அழகான நினைவுகளாக மாற்ற, புகைப்படம் எல்லாம் எடுத்தோம்.',
       'HH1':'முதல் சந்திப்பு ',
       'HH2':'ஹலோ ஹலோ',
       'HH3':'காதல் பரிசு',
@@ -200,12 +200,12 @@ z.constant('languages',{
 'r41': 'மந்திரம் - கோமதி',
 'r42': 'அண்ணன் - அண்ணி',
 
-'r33': '(அத்தான்) முதல்ல வேணாம்னு சொன்ன மாப்பிளை பொண்ண பார்த்துட்டு வந்த அப்புறம் அந்தர் பல்டி அடிச்சு டூயட் பாட ஆரம்பிச்சுட்டான்.என் மாப்பிளைக்கு நல்ல வாழ்க்கையும் எனக்கு ஒரு புது தங்கச்சியும் கிடைச்சுட்டானு ரொம்ப சந்தோசம். (தங்கச்சி)என் செல்ல அண்ணனுக்கு கல்யாணம் அவனுக்கு எப்போதும் நான்தான் செல்ல ராஜகுமாரி.ஆனா இப்போம் அவனுக்கான ராஜகுமாரி வந்தாச்சு. இனி அண்ண பாசத்தோட அண்ணி பாசமும் கிடைக்க பொது இந்த ராஜகுமாரிக்கு!!!',
-'r43': '(அண்ணன்) என் தம்பிக்கு பெஸ்ட் வாழ்க்கை கிடைச்சதுல ரொம்ப ரொம்ப சந்தோசம், கோமதி வந்த அப்புறம் எங்க வீட்ல நடக்க போற முதல் பெரிய விழா, அதுனால  நாங்க ரொம்ப ஜாலியாவும் இருக்கோம்.. (அண்ணி) இவ்ளோ நாள் அண்ணியா பார்த்துக்கிட்ட எனக்கு இப்போம் அக்காங்கிற பதவி உயர்வு கிடைச்சாச்சு.',
+'r33': '(அத்தான்) முதல்ல வேணாம்னு சொன்ன மாப்பிளை பொண்ண பார்த்துட்டு வந்த அப்புறம் அந்தர் பல்டி அடிச்சு டூயட் பாட ஆரம்பிச்சுட்டான். என் மாப்பிளைக்கு நல்ல வாழ்க்கையும் எனக்கு ஒரு புது தங்கச்சியும் கிடைச்சுட்டானு ரொம்ப சந்தோசம். (தங்கச்சி)என் செல்ல அண்ணனுக்கு கல்யாணம் அவனுக்கு எப்போதும் நான்தான் செல்ல ராஜகுமாரி.ஆனா இப்போ அவனுக்கான ராஜகுமாரி வந்தாச்சு. இனி அண்ண பாசத்தோட அண்ணி பாசமும் கிடைக்க பொது இந்த ராஜகுமாரிக்கு!!!',
+'r43': '(அண்ணன்) என் தம்பிக்கு பெஸ்ட் வாழ்க்கை கிடைச்சதுல ரொம்ப ரொம்ப சந்தோசம், கோமதி வந்த அப்புறம் எங்க வீட்ல நடக்க போற முதல் பெரிய விழா, அதுனால  நாங்க ரொம்ப ஜாலியாவும் இருக்கோம்.. (அண்ணி) இவ்ளோ நாள் அண்ணியா பார்த்துக்கிட்ட எனக்கு இப்போ அக்காங்கிற பதவி உயர்வு கிடைச்சாச்சு.',
 
 'r51': ' அருண் கல்யாண்',
 'r52': 'சகோதரன்',
-'r53': 'இவ்ளோ நாள் அக்காகிட்ட  மாட்டிகிட்டு இருந்த நான் இப்போம் தப்புச்சிட்டன், ஆனா அத்தான் அவங்களா வந்து இப்படி மாட்டிக்கிட்டாங்க. அவளை நீங்க பார்த்துகிறதவிட, உங்கள நீங்க அவட்ட இருந்து பத்திரமா பார்த்துக்கோங்க அத்தான். என்னதான் இப்படி விளையாட்ட பேசுனாலும் நிஜமா அவளை தேடும், சண்டை போட இனி ஆள் இருக்காதுலா...',
+'r53': 'இவ்ளோ நாள் அக்காகிட்ட  மாட்டிகிட்டு இருந்த நான் இப்போ தப்புச்சிட்டேன், ஆனா அத்தான் அவங்களா வந்து இப்படி மாட்டிக்கிட்டாங்க. அவளை நீங்க பார்த்துகிறதவிட, உங்கள நீங்க அவட்ட இருந்து பத்திரமா பார்த்துக்கோங்க அத்தான். என்னதான் இப்படி விளையாட்ட பேசுனாலும் நிஜமா அவளை தேடும், சண்டை போட இனி ஆள் இருக்காதுலா...',
 
 'e0':'இதயங்கள் இணைகையில் இனிமைகள் வாழ்விலே! உன் இதயமும் என் இதயமும் ஒன்றாக சேரட்டுமே !.....',
 'e1':'திருமண வைபவம்',
@@ -214,14 +214,14 @@ z.constant('languages',{
 'edate':'செப்டம்பர் 4',
 
 'e11':'இந்த செப்டம்பர் மாசம் 4ஆம் தேதி திங்ககிழம காலையிலே 6.00 மணிக்கு மேல 7.25 மணிக்குள்ள எங்களுக்கு கல்யாணம். பாசமா கூப்பிடுறோம். குடும்பமா வாங்க. வாழ்த்துக்களையும் ஆசிகளையும் தாங்க. \n\n\n இடம்: சிவா கல்யாண மண்டபம், தென்காசி.',
-'e12':'எங்க கல்யாணத்தோட சந்தோசத்தை குழந்தைகள் கூடவும், பெரியவங்க கூடவும், கொண்டாடி அவங்க ஆசீர்வாதம் பெற்று, நாங்க எங்க வாழ்க்கையின் அத்தியாயம் இனிதே துவக்குறோம். \n\n\n இடம்: பிரணவ் ஆசிரமம், குற்றாலம்.',
-'e13':'நெற்றியில்  திலகம் துலங்கிட பற்றிய கைவளையல்கள் குளுங்கிட சுற்றிலும் உள்ளவர்வகள் மகிழ்ந்திட பெற்றவர்கள் உள்ளம் குளிர்ந்திட சீனுராஜி யின் காதல் விளையாட்டு இனிதே ஆரம்பம்..\n வரவேற்பு மாலை 6 மணி முதல் 7 மணி வரை. நலுங்கு 7 மணிக்கு மேல். \n இடம்: சிவா கல்யாண மண்டபம், தென்காசி.',
+'e12':'எங்க கல்யாணத்தோட சந்தோசத்தை குழந்தைகள் கூடவும், பெரியவங்க கூடவும், கொண்டாடி அவங்க ஆசிர்வாதத்தோட எங்களின் வாழ்க்கை அத்தியாயம் இனிதே ஆரம்பம்  \n\n\n இடம்: பிரணவ ஆசிரமம், குற்றாலம்.',
+'e13':'நெற்றியில் திலகம் துலங்கிட பற்றிய கைவளையல்கள் குளுங்கிட சுற்றிலும் உள்ளவர்வகள் மகிழ்ந்திட பெற்றவர்களின் உள்ளம் குளிர்ந்திட சீனுராஜி யின் காதல் விளையாட்டு இனிதே ஆரம்பம்..\n வரவேற்பு - மாலை 6 மணி முதல் 7 மணி வரை. \n நலுங்கு - 7 மணிக்கு மேல். \n இடம்: சிவா கல்யாண மண்டபம், தென்காசி.',
 
 "rs1":"காதலும் கவிதையுமான சங்கமம், நேசித்த இரு மனங்களின் வாழ்வினுக்கான காதல் துவக்கம். \n இனிய விடியலாய்  திருமண விழாக்கோலம், இனி வாழும் வாழ்வே விழா காணும்.",
 "rs2":"எங்களின் சந்தோஷத்தை  பகிரவும், உங்களது வாழ்த்துக்களையும், ஆசிகளையும் பெறுவதற்கான நம் சந்திப்பு இனிமையாக நிகழ, கீழே மறக்காமல் பதில் அளிக்கவும்.",
 "rs3":"மகிழ்வான தருணங்கள் மலர்ந்தது இனிமையாக... நெகிழ்வான இரு குடும்பங்கள் இணைந்தது பாசமாக...",
-"rs4":"அன்பாய் பார்த்து கொள்ள நீயும், அக்கறையாக பார்த்து கொள்ள அவளும், திருமணத்தில் வாழ்த்துக்களை சுமந்து பூக்களாய் தூவி வாழ்த்துகிறோம்...",
-"rs5":"எங்களின் மகிழ்வான தருணங்கள் உங்கள் வருகையால் மலரட்டும் இனிமையாக, நெகிழ்வான நேசங்கள் நிகழட்டும் இளமையாக !... \n  எங்கள் திருமண விழாவை அலங்கரிக்க உங்களை அன்போடு அழைக்கிறோம். உங்களது வருகையை மறக்காமல் இங்கு பதிவு செய்யவும்",
+"rs4":"அன்பாய் பார்த்து கொள்ள நீயும், அக்கறையாக பார்த்து கொள்ள அவளும், இணையும் திருமணத்தில் வாழ்த்துக்களை சுமந்து பூக்களாய் தூவி வாழ்த்துகிறோம்...",
+"rs5":"எங்களின் மகிழ்வான தருணங்கள் உங்களது வருகையினால் மலரட்டும் இனிமையாக!... \n உங்களது  வாழ்த்துக்கள் என்னும் கொண்டு அலங்கரிக்க அன்போடு அழைக்கிறோம். உங்களது வருகையை மறக்காமல் இங்கு பதிவு செய்யவும்", 
 
 "fng_title":"விருந்தினர் எண்ணிக்கை",
 "fng1":"1 நபர்",
